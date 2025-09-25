26b5d663c3901a273b43d3632a46355293a41b45
Mohamed LAHCEN <mohamed.lahcen@exalt-company.com>
2025-09-25 14:40:46 +0200

    feat: initial landing page avec Home/Pricing/FAQ

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./features/home/home').then(m => m.HomeComponent),
+  },
+  {
+    path: 'pricing',
+    loadComponent: () =>
+      import('./features/pricing/pricing').then(m => m.PricingComponent),
+  },
+  {
+    path: 'faq',
+    loadComponent: () =>
+      import('./features/faq/faq').then(m => m.FaqComponent),
+  },
+  { path: '**', redirectTo: '' },
+];
